9c65a4986b9714145d22d6624a16872b77f6f643
KShivendu <kshivendu1@gmail.com>
2023-06-14 11:27:47 +0530

    Fix results dir permissions

--- experiment.py
+++ experiment.py
@@ -15,10 +15,12 @@ if __name__ == "__main__":
     parser.add_argument("--delete-existing", action="store_true", help="If set, existing results will be deleted")
     args = parser.parse_args()
 
-    if args.delete_existing: # or just set this to True
-        results_dir = Path("results")
+    results_dir = Path("results")
+
+    if args.delete_existing:
         results_dir.unlink(missing_ok=True)
-        results_dir.mkdir(exist_ok=True)
+
+    results_dir.mkdir(exist_ok=True)
 
     # num_vectors = [1k, 2k, ..., 10k, 250k, ~500k]
     num_vectors = list(range(1, 11, 1)) + [100, 250, 486]
